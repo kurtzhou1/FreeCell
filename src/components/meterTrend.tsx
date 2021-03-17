823d044fdcacfd024da35134459eb1570e3699f9
Kurt <pandabat1@gmail.com>
2021-03-17 16:49:29 +0800

    增加時間

--- src/components/meterTrend.tsx
+++ src/components/meterTrend.tsx
@@ -1,8 +1,9 @@
 import React,{useState,useEffect} from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import dayjs from 'dayjs'
 
 const MeterTrend:React.FC<any> = (props) => {
-
+  
   const dataKey = Object.keys(props.data);
   const [dataArray, setDataArray] = useState<any[]>([]);
 
@@ -10,7 +11,12 @@ const MeterTrend:React.FC<any> = (props) => {
     if(dataKey.length !== 0){
       let nine = props.data.parameters['9'].value; //庫內溫度
       let ten = props.data.parameters['10'].value; //蒸發器溫度
-      let tmpArr = {Warehouse_Temperature:nine,Evaporator_Temperature:ten};
+      let timestamp = props.data.timestamp;
+      let timestampDate = dayjs(timestamp).format('YYYY-MM-DD');
+      let timestampMin = dayjs(timestamp).format('HH:mm:ss');
+      let tmpArr = {title:`${timestampDate} ${timestampMin}`,Warehouse_Temperature:nine,Evaporator_Temperature:ten};
+    
+  //2021-03-17T07:48:18.656
       if(dataArray.length <= 19){
         setDataArray([...dataArray,tmpArr ]);
       }else{
@@ -23,17 +29,18 @@ const MeterTrend:React.FC<any> = (props) => {
   
   return(
       <LineChart
-          width={1000}
+          width={1400}
           height={500}
           data={dataArray}
           margin={{
           top: 30,
           right: 20,
           left: 20,
-          bottom: 5
+          bottom: 50
           }}
       >
           <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="title" angle={10}/>
           <YAxis />
           <Tooltip />
           <Legend />
